20c310bb1b7310978d2a41b39fe6ff06fbe2364b
Drew Hamilton <drew.hamilton@backbase.com>
2018-12-28 10:30:07 +0100

    Implement sharing via FileProvider

--- app/src/main/java/com/google/samples/dataprivacy/page/viewimage/ImageViewerActivity.java
+++ app/src/main/java/com/google/samples/dataprivacy/page/viewimage/ImageViewerActivity.java
@@ -63,6 +63,10 @@ public class ImageViewerActivity extends AppCompatActivity implements ImageShare
      */
     @Override
     public void shareImage(String path) {
-        Toast.makeText(this, "TODO: implement sharing.", Toast.LENGTH_SHORT).show();
+        final Uri contentUri = FileProvider.getUriForFile(this, "com.google.samples.dataprivacy.FileProvider", new File(path));
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setDataAndType(contentUri, "image/png");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
     }
 }
